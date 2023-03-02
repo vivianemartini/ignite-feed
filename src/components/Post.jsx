569c1626f5361ab77b8700345177288b8005c8e2
Viviane Martini <martini.viviane@gmail.com>
2023-03-02 14:05:08 -0300

    add form, property focus and focus:whitin

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -20,11 +20,22 @@ export function Post() {
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p>👉 <a href="#">jane.design/doctorcare</a></p>
                 <p> 
-                    <a href=""> #novoprojeto</a> 
-                    <a href=""> #nlw</a> 
-                    <a href=""> #rocketseat</a>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '} 
+                    <a href="">#rocketseat</a>
                 </p> 
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu comentário</strong>
+
+                <textarea placeholder='Deixe seu comentário'/>
+
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+    
+            </form>
         </article>
     );
   }
